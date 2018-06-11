15d053c81c791f0b241fa2e2adf0aa159fc5d0de
Simon Roberts <simon@dancingcloudservices.com>
2018-06-11 12:58:34 -0600

    End of day one, accessibility questions for overriding

---
+++ src/main/java/access/X.java
@@ -0,0 +1,14 @@
+package access;
+
+public class X {
+  protected void doStuff() {}
+}
+class A extends X {
+  void doStuff() {}
+}
+class B extends X {
+  public void doStuff() {}
+}
+class C extends X {
+  private void doStuff(){}
+}
---
+++ src/main/java/access2/X.java
@@ -0,0 +1,14 @@
+package access2;
+
+public class X {
+  public void doStuff() {}
+}
+class A extends X {
+  void doStuff() {}
+}
+class B extends X {
+  protected void doStuff() {}
+}
+class C extends X {
+  private void doStuff() {}
+}
