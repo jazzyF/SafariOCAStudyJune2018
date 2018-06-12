deaf8c82d660aaa70059a74e37ac3234a81e4ca2
Simon Roberts <simon@dancingcloudservices.com>
2018-06-12 09:16:02 -0600

    Liskov substitution with return types

---
+++ src/main/java/liskovreturns/Ex1.java
@@ -0,0 +1,21 @@
+package liskovreturns;
+
+import java.time.LocalDate;
+
+class X {
+  public CharSequence doStuff() { return null; }
+}
+class A extends X {
+  public String doStuff() {  return null; }
+}
+class B extends X {
+  public StringBuilder doStuff() { return null; }
+}
+class C extends X {
+  public LocalDate doStuff() { return null; }
+}
+class D extends X {
+  public Object doStuff() { return null; }
+}
+public class Ex1 {
+}
---
+++ src/main/java/liskovreturns/Ex2.java
@@ -0,0 +1,18 @@
+package liskovreturns;
+
+import java.time.LocalDate;
+
+class Y {
+  public int doStuff() { return 1; }
+}
+class P extends Y {
+  // surprise! assignment compatibilty only allowed
+  // for object return types...
+  public short doStuff() {  return 1; }
+}
+class Q extends Y {
+  // No surprise
+  public long doStuff() { return 1L; }
+}
+public class Ex2 {
+}
