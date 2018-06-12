a861d2c67773bfdafe0b42cfcdd681545802ea6e
Simon Roberts <simon@dancingcloudservices.com>
2018-06-12 13:03:48 -0600

    Constuctors, parent initialization, pass by value discussion

---
+++ src/main/java/construction/Q26Base.java
@@ -0,0 +1,22 @@
+package construction;
+
+public class Q26Base {
+  int x = 99;
+// default not provided because we have an explicit constructor declared
+  Q26Base() {
+    System.out.println("zero arg constructor for base");
+  }
+  Q26Base(int x){ System.out.println("in base constructor x is " + x); }
+}
+class Q26Sub extends Q26Base {
+  Q26Sub() {
+    this(99); // passes control across.
+    System.out.println("hello");
+  }
+  
+  Q26Sub(int x) {
+//    super();// implicit call fails! no Q26Base() no arg constructor...
+    super(x + 13);
+    System.out.println("in sub constructor, x is " + x);
+  }
+}
