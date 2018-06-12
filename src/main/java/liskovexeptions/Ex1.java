314aee445498b746440baf9366c62cde68ef9da1
Simon Roberts <simon@dancingcloudservices.com>
2018-06-12 08:51:03 -0600

    Exception discussion, and Liskov substitution with checked exceptions.

---
+++ src/main/java/liskovexeptions/Ex1.java
@@ -0,0 +1,26 @@
+package liskovexeptions;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+class X {
+  public void doStuff() throws IOException {}
+}
+
+class A extends X {
+  public void doStuff() throws FileNotFoundException {}
+}
+
+class B extends X {
+  public void doStuff() throws RuntimeException {}
+}
+
+class C extends X {
+  public void doStuff() {}
+}
+
+class D extends X {
+  public void doStuff() throws Exception {}
+}
+public class Ex1 {
+}
